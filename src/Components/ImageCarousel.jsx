5add3dabcd470c6b799e988a5ccfd633055e3e21
Nihad Mammadli <nihadmammadli03@gmail.com>
2024-11-26 15:16:22 +0400

    Redesign for phones done

--- src/Components/ImageCarousel.jsx
+++ src/Components/ImageCarousel.jsx
@@ -1,53 +1,74 @@
-import Slider from 'react-slick';
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import style from './style.module.scss';
-import { images } from '../Assets/Rotate';
-import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import style from "./style.module.scss";
+import { images } from "../Assets/Rotate";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import React, { useState, useEffect } from "react";
 
 const ImageCarousel = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     };
 
-    function SampleNextArrow(props) {
-        const { onClick } = props;
-        return (
-            <div className={style.slickNext} onClick={onClick}>
-                <RightOutlined />
-            </div>
-        );
-    }
+    window.addEventListener("resize", handleResize);
 
-    function SamplePrevArrow(props) {
-        const { onClick } = props;
-        return (
-            <div className={style.slickPrev} onClick={onClick} >
-                <LeftOutlined />
-            </div>
-        );
-    }
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow:
+      screenSize?.width < 768 ? 1 : screenSize?.width < 1024 ? 2 : screenSize?.width > 1024 ? 3 : 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
+  function SampleNextArrow(props) {
+    const { onClick } = props;
     return (
-        <div className={style.SliderMain}>
-            <Slider {...settings}>
-                {images.map((image, index) => (
-                    <div key={index}>
-                        <img src={image} alt={`Slide ${index}`} className={style.image} />
-                    </div>
-                ))}
-            </Slider>
-        </div>
-
+      <div className={style.slickNext} onClick={onClick}>
+        <RightOutlined />
+      </div>
     );
+  }
+
+  function SamplePrevArrow(props) {
+    const { onClick } = props;
+    return (
+      <div className={style.slickPrev} onClick={onClick}>
+        <LeftOutlined />
+      </div>
+    );
+  }
+
+  return (
+    <div className={style.SliderMain}>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Slide ${index}`} className={style.image} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default ImageCarousel;
--- src/Layouts/Footer/index.jsx
+++ src/Layouts/Footer/index.jsx
@@ -1,9 +1,11 @@
-
-
-
-import style from './style.module.scss';
-import { FacebookOutlined, TwitterOutlined, InstagramOutlined, LinkedinOutlined } from '@ant-design/icons';
-import { Layout, Typography, Input, Button, Row, Col } from 'antd';
+import style from "./style.module.scss";
+import {
+  FacebookOutlined,
+  TwitterOutlined,
+  InstagramOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
+import { Layout, Typography, Input, Button, Row, Col } from "antd";
 
 const { TextArea } = Input;
 const { Title, Text } = Typography;
@@ -13,30 +15,41 @@ const Index = () => {
   return (
     <Footer className={style.Footer}>
       <Row gutter={[40, 0]} className={style.FooterRow}>
-        <Col span={6}>
-          <Title level={3} className={style.FooterTitle}>
+        <Col xs={18} sm={18} md={12} lg={6}>
+          <Text level={3} className={style.FooterTitle}>
             Contact Us
-          </Title>
-          <Input style={{ borderRadius: "0px", margin: '10px 0' }} placeholder="Name Surname" />
-          <Input placeholder="Email" style={{ borderRadius: "0px", margin: '10px 0' }} />
-          <TextArea style={{ borderRadius: "0px", margin: '10px 0' }} rows={3} placeholder="Your Message" />
-          <Button style={{ backgroundColor: "Black", color: "white", border: "none", borderRadius: "0px", width: "100%", marginTop: '10px' }}>Send</Button>
+          </Text>
+          <Input style={{ borderRadius: "0px", margin: "10px 0" }} placeholder="Name Surname" />
+          <Input placeholder="Email" style={{ borderRadius: "0px", margin: "10px 0" }} />
+          <TextArea
+            style={{ borderRadius: "0px", margin: "10px 0" }}
+            rows={3}
+            placeholder="Your Message"
+          />
+          <Button
+            style={{
+              backgroundColor: "Black",
+              color: "white",
+              border: "none",
+              borderRadius: "0px",
+              width: "100%",
+              marginTop: "10px",
+            }}
+          >
+            Send
+          </Button>
         </Col>
-        <Col span={5}>
+        <Col xs={24} sm={24} md={12} lg={5}>
           <Row>
-            <Title level={3} className={style.FooterTitle}>
+            <Text level={3} className={style.FooterTitle}>
               Contact Info
-            </Title>
-          </Row>
-          <Row>
-            <Text className={style.FooterText}>
-              info@nizamiganjavi-ic.org
             </Text>
           </Row>
           <Row>
-            <Text className={style.FooterText}>
-              +994 12 597 48 65
-            </Text>
+            <Text className={style.FooterText}>info@nizamiganjavi-ic.org</Text>
+          </Row>
+          <Row>
+            <Text className={style.FooterText}>+994 12 597 48 65</Text>
           </Row>
           <Row>
             <Text className={style.FooterText}>
@@ -44,21 +57,17 @@ const Index = () => {
             </Text>
           </Row>
         </Col>
-        <Col span={5}>
+        <Col xs={24} sm={24} md={12} lg={5}>
           <Row>
-            <Title level={3} className={style.FooterTitle}>
+            <Text level={3} className={style.FooterTitle}>
               About Us
-            </Title>
-          </Row>
-          <Row>
-            <Text className={style.FooterText}>
-              info@nizamiganjavi-ic.org
             </Text>
           </Row>
           <Row>
-            <Text className={style.FooterText}>
-              +994 12 597 48 65
-            </Text>
+            <Text className={style.FooterText}>info@nizamiganjavi-ic.org</Text>
+          </Row>
+          <Row>
+            <Text className={style.FooterText}>+994 12 597 48 65</Text>
           </Row>
           <Row>
             <Text className={style.FooterText}>
@@ -66,21 +75,17 @@ const Index = () => {
             </Text>
           </Row>
         </Col>
-        <Col span={5}>
+        <Col xs={24} sm={24} md={12} lg={5}>
           <Row>
-            <Title level={3} className={style.FooterTitle}>
+            <Text level={3} className={style.FooterTitle}>
               Publications
-            </Title>
-          </Row>
-          <Row>
-            <Text className={style.FooterText}>
-              info@nizamiganjavi-ic.org
             </Text>
           </Row>
           <Row>
-            <Text className={style.FooterText}>
-              +994 12 597 48 65
-            </Text>
+            <Text className={style.FooterText}>info@nizamiganjavi-ic.org</Text>
+          </Row>
+          <Row>
+            <Text className={style.FooterText}>+994 12 597 48 65</Text>
           </Row>
           <Row>
             <Text className={style.FooterText}>
@@ -88,15 +93,15 @@ const Index = () => {
             </Text>
           </Row>
         </Col>
-        <Col span={3}>
-          <Title level={3} className={style.FooterTitle}>
+        <Col xs={24} sm={24} md={12} lg={3}>
+          <Text level={3} className={style.FooterTitle}>
             Follow Us
-          </Title>
-          <div style={{ display: 'flex', gap: '10px' }}>
-            <FacebookOutlined style={{ fontSize: '24px' }} />
-            <TwitterOutlined style={{ fontSize: '24px' }} />
-            <InstagramOutlined style={{ fontSize: '24px' }} />
-            <LinkedinOutlined style={{ fontSize: '24px' }} />
+          </Text>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <FacebookOutlined style={{ fontSize: "24px" }} />
+            <TwitterOutlined style={{ fontSize: "24px" }} />
+            <InstagramOutlined style={{ fontSize: "24px" }} />
+            <LinkedinOutlined style={{ fontSize: "24px" }} />
           </div>
         </Col>
       </Row>
